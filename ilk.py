1f461edd4e20e93fd6d7a0389913c5b449ac5334
İbrahim EDİZ <edizdesign@gmail.com>
2023-08-15 07:00:17 +0000

    15082023_2

--- ilk.py
+++ ilk.py
@@ -2,8 +2,9 @@ liste = ["Zafer","Cengiz","Duygu","Suleyman","Aytekin","Semih","Hanifi",
 "Emin","Mehmet","Ayşe","Demet","Seda"]
 
 import os 
-fileName = "ilk.ipynb"
+
 for item in liste:
+    fileName = f"01_pandas_{item}.ipynb"
     if not os.path.exists(os.path.join("Exercises",item)):
         os.mkdir(os.path.join("Exercises",item))
     open(os.path.join("Exercises",item,fileName),"w")
